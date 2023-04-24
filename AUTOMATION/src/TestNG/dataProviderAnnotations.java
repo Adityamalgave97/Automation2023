0e8cd0bc0c9e5648b383b4d159f44f30daa8eec7
HP <HP@192.168.2.205>
2023-04-24 23:28:50 +0530

    data provider annotation

--- AUTOMATION/src/TestNG/dataProviderAnnotations.java
+++ AUTOMATION/src/TestNG/dataProviderAnnotations.java
@@ -4,6 +4,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class dataProviderAnnotations {
+	
  
   @DataProvider(name="data-provider")
   public Object [][] dataProviderMethod(){
